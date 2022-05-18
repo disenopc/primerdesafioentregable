ebb937a4f9461925146312c0c49c9f3f2ba2c416
disenopc <capitanellipl@gmail.com>
2022-05-18 16:57:14 -0300

    elimine comentarios extra, limpie el codigo

--- codigo.js
+++ codigo.js
@@ -1,17 +1,15 @@
 //Ciclos e Iteraciones
 
 //EJEMPLO usando while para registro de ingresos a una oficina. Aquellos que no tengan carnet de vacunación completo contra covid no podran ingresar.
-let nombreEmpleado = prompt("Ingrese su nombre");
+let nombreEmpleado = prompt("Registro de ingreso a la oficina: Ingrese su nombre completo");
 let ingresarDosis = parseInt(prompt("Ingrese cantidad de dosis aplicadas"));
 empleado = "";
 while ((nombreEmpleado != "") && (ingresarDosis >= "2")) {
     alert("Bienvenido/a " + nombreEmpleado + ".");
-    nombreEmpleado = prompt("Ingrese su nombre");
+    console.log("Nombre del Empleado: " + nombreEmpleado + ", " + ingresarDosis + "dosis")
+    nombreEmpleado = prompt("Registro de ingreso a la oficina: Ingrese su nombre completo");
     ingresarDosis = parseInt(prompt("Ingrese cantidad de dosis aplicadas"));
     empleado = empleado + nombreEmpleado;
 }
-console.log("Usuario No Autorizado" + ": " + nombreEmpleado + ", " + ingresarDosis + " dosis");
+console.log("Ingreso no permitido" + ": " + nombreEmpleado + ", " + ingresarDosis + " dosis");
 alert("Usuario No Autorizado, corresponde aplicarse una dosis más para ingresar al establecimiento");
-
-
-//EJEMPLO usando for para registro
