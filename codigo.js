1346edc29ed2e0308265dc4bd5da2c12f4b18315
disenopc <capitanellipl@gmail.com>
2022-05-18 17:19:21 -0300

    agregue variable empleado para almacenar el registro

--- codigo.js
+++ codigo.js
@@ -3,13 +3,14 @@
 //EJEMPLO usando while para registro de ingresos a una oficina. Aquellos que no tengan carnet de vacunación completo contra covid no podran ingresar.
 let nombreEmpleado = prompt("Registro de ingreso a la oficina: Ingrese su nombre completo");
 let ingresarDosis = parseInt(prompt("Ingrese cantidad de dosis aplicadas"));
-empleado = "";
+let empleado = "";
 while ((nombreEmpleado != "") && (ingresarDosis >= "2")) {
     alert("Bienvenido/a " + nombreEmpleado + ".");
-    console.log("Nombre del Empleado: " + nombreEmpleado + ", " + ingresarDosis + "dosis")
+    console.log("Ingreso permitido: " + nombreEmpleado + ", " + ingresarDosis + "dosis");
+    empleado = empleado + nombreEmpleado;
     nombreEmpleado = prompt("Registro de ingreso a la oficina: Ingrese su nombre completo");
     ingresarDosis = parseInt(prompt("Ingrese cantidad de dosis aplicadas"));
-    empleado = empleado + nombreEmpleado;
+
 }
 console.log("Ingreso no permitido" + ": " + nombreEmpleado + ", " + ingresarDosis + " dosis");
 alert("Usuario No Autorizado, corresponde aplicarse una dosis más para ingresar al establecimiento");
